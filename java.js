b06381783af4c39083b80706f9cd918370fa1447
Laurareis1 <116307113+Laurareis1@users.noreply.github.com>
2022-10-28 15:59:03 +0100

    exerccicio3

--- java.js
+++ java.js
@@ -1,16 +1,47 @@
 function welcome (name){
-  var texto ="hello! how are you";
-  var C = texto.concat( name);
-  //outras formas de fazer 
- // console.log(name.concat(texto))
- //let C = hello! how are you ${name}?
- return C;
-}
- 
-function myConcat(lista,appendvalue){
-    for(pos in lista) {
-      lista[pos] = lista[pos] + apppendvalue;
-    }
 
+    var texto ="hello! how are you";
+    var C = texto.concat( name);
+    //outras formas de fazer 
+   // console.log(name.concat(texto))
+   //let C = hello! how are you ${name}?
+   return C;
+}
+
+ 
+function myConcat(lista,parametro) {
+
+    for(pos in lista) {
+      lista[pos] = lista[pos] + parametro;
+    }
     return lista;
 }
+
+function compara (lista,operacao){
+    let total = 0;
+    if(operacao == '+'){
+        for( value of lista){
+            total +=value;
+        }
+        
+    }
+       
+    else if (operacao =='-'){
+        for( value of lista){
+            total -=value;
+        }
+
+    }
+    
+    else if (operacao == '*'){
+        for( value of lista){
+            total *=value;
+        }
+
+    }
+    return total;
+}
+
+
+
+
