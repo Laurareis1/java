7fe2e46c69a39ef24d97ef36a3cc8289f667e9a7
Laurareis1 <116307113+Laurareis1@users.noreply.github.com>
2022-10-31 15:12:19 +0000

    31

--- java.js
+++ java.js
@@ -146,5 +146,66 @@ function eliminar (name){
     }
     delete phoneBook[name];
     return phoneBook
+}
 
+function geral (name,number,operacao){
+    let phoneBook = {
+        Abel: 5802943,
+        Laura: 9761405,
+        Lisa: 5850628,
+        Adilson: 9240912,
+        Victor: 9519228
+    }
+    if(operacao== 'elimina'){
+        delete phoneBook[name];
+        return phoneBook;
+    } 
+     else if (operacao== 'adicionar'){
+        phoneBook[name] = number
+        return phoneBook;
+   }
+   else if(operacao== 'pesquisar')
+   return phoneBook[name];
+
+}
+function ChamarFuncao(name,number,operacao){
+
+    if(operacao== 'elimina'){
+       return eliminar (name);
+    } 
+     else if (operacao== 'adicionar'){
+        return AddPhoneNumber(name, number);
+   }
+   else if(operacao== 'pesquisar')
+    return giveMePhone(name);
+}
+
+
+var phoneBook={
+    Abel :5802943,
+    Laura : 9761405,
+    Lisa : 5850628,
+    Adilson : 9240912,
+    Victor : 9519228
+}
+function eliminar1(name, phoneBook){
+    delete phoneBook[name];
+    return phoneBook
+}
+function adicionar1 (name, number,phoneBook){
+    phoneBook[name] = number
+    return phoneBook
+}
+function pesquisar1 (name,phoneBook){
+    return phoneBook[name];
+}
+function todos(name,number,operacao,phoneBook){
+    if(operacao== 'elimina'){
+        return eliminar1(name, phoneBook);
+     } 
+      else if (operacao== 'adicionar'){
+         return adicionar1 (name,number,phoneBook);
+    }
+    else if(operacao== 'pesquisar')
+     return pesquisar1 (name,phoneBook);
 }
