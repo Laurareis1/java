1e7dbf0ebbb219d55b988ce0938ef28a75210f1b
Laurareis1 <116307113+Laurareis1@users.noreply.github.com>
2022-11-02 13:08:00 +0000

    inicio string

--- string java/string.js
+++ string java/string.js
@@ -1,3 +1,27 @@
 function inicioString (str1,str2){
 console.log (str1.startsWith(str2))
 }
+
+function inicioString1 (str1,str2){
+  let i=1;
+  for (;i<str2.length;
+       i++){
+           if(str1.substring(0,str2.length)==str2){
+             return true;
+           } else{
+            return false;
+           }
+          }     
+}
+function inicioString2 (str1,str2){
+ resul=0;
+ for (i=0;i<str2.length-1;
+  i++){
+    if(str2[i]==str1[i]){
+        resul=true;
+    }else if (str2[i]!=str1[i]){
+      resul=false;
+    }
+  }
+  return resul;
+}
