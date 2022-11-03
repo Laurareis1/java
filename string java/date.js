67f8c5199d6a91c87aa9df003fd1b7dc06d1152e
Laurareis1 <116307113+Laurareis1@users.noreply.github.com>
2022-11-03 18:03:02 +0000

    primeira parte

--- string java/date.js
+++ string java/date.js
@@ -6,5 +6,14 @@ function ex9 (hora){
 function ex9v2(hora){
     let data = new Date();
     let dat =data.setHours(data.getHours() + hora)
-    return data + dat;
+    return data ;
 }
+function finalDeSemana (){
+    const dias = ["segunda","terça","quarta","quinta","Sexta","sabado","Domingo"];
+    let da= new Date();
+    let dia= dias[da.getDay()];
+    let year= da.getFullYear();
+    let mes =da.getMonth();
+    let data= dia+se+mes+se+year;
+    return data;
+    }
