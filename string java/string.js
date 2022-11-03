2376cbb8339aa1b5d876d1cf14fdee9a98501f04
Laurareis1 <116307113+Laurareis1@users.noreply.github.com>
2022-11-03 14:50:43 +0000

    string

--- string java/string.js
+++ string java/string.js
@@ -3,19 +3,17 @@ console.log (str1.startsWith(str2))
 }
 
 function inicioString1 (str1,str2){
-  let i=1;
-  for (;i<str2.length;
-       i++){
-           if(str1.substring(0,str2.length)==str2){
+           if(str1.substring(0,str2.length-1)==str2){
              return true;
            } else{
             return false;
            }
-          }     
+         
 }
 function inicioString2 (str1,str2){
- resul=0;
- for (i=0;i<str2.length-1;
+ let resul;
+ for (i=0;
+  i<str2.length;
   i++){
     if(str2[i]==str1[i]){
         resul=true;
@@ -25,3 +23,60 @@ function inicioString2 (str1,str2){
   }
   return resul;
 }
+
+function concatString(str,num=1){
+let resul='';
+  for(i=1;
+    i<=num;
+    i++){
+    resul+=str
+  }
+  return resul;
+}
+
+function concatstring1 (str,num=1){
+let result='';
+for(i=0;
+  i<=num;
+  i++){
+  result=str.concat(str)
+  }
+return result;
+  }
+
+  /*function divideCarater(str,num=1){
+    console.log(chunkString(str,num))
+  }*/
+
+  function chunkString(msg, divide=1){
+    const chunks = [];
+    if(divide==1){
+        chunks.push(msg);
+        return chunks;
+    }
+    let part="";
+    for(let i=0;i<msg.length;i++){
+        part=part+msg[i];            
+        if(part.length==divide){
+            chunks.push(part);
+            part='';
+        }
+    }
+    chunks.push(part);
+    return chunks;
+}
+
+function chunkString1 (str,num=1) {
+
+  const size = Math.ceil(str.length/num)
+  const r = Array(size)
+  let offset = 0
+  
+  for (let i = 0; i < size; i++) {
+    r[i] = str.substr(offset,num)
+    offset += num
+  }
+  
+  return r
+}
+
